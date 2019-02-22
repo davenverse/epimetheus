67875ccc8c9af7035b9960e33b605240d84c8f33
Christopher Davenport <Chris@ChristopherDavenport.tech>
2019-02-21 22:19:10 -0500

    Add Initial Timing Methodology

--- core/src/main/scala/io/chrisdavenport/epimetheus/Histogram.scala
+++ core/src/main/scala/io/chrisdavenport/epimetheus/Histogram.scala
@@ -2,15 +2,21 @@ package io.chrisdavenport.epimetheus
 
 import cats.implicits._
 import cats.effect._
+import cats.implicits._
 import io.prometheus.client.{Histogram => JHistogram}
+import scala.concurrent.duration.TimeUnit
 
-final class Histogram[F[_]: Sync] private (private val h: JHistogram.Child){
+final class Histogram[F[_]: Sync: Clock] private (private val h: JHistogram.Child){
   def observe(d: Double): F[Unit] = Sync[F].delay(h.observe(d))
+  def timed[A](fa: F[A], unit: TimeUnit): F[A] = for {
+    start <- Clock[F].monotonic(unit)
+    out <- Sync[F].guarantee(fa)(Clock[F].monotonic(unit).flatMap(now => observe((now - start).toDouble)))
+  } yield out
 }
 
 object Histogram {
 
-  def buildBuckets[F[_]: Sync](cr: CollectorRegistry, name: String, help: String, buckets: Double*): F[Histogram[F]] = for {
+  def buildBuckets[F[_]: Sync: Clock](cr: CollectorRegistry, name: String, help: String, buckets: Double*): F[Histogram[F]] = for {
     c <- Sync[F].delay(JHistogram.build().name(name).help(help).buckets(buckets:_*))
     out <- Sync[F].delay(c.register(CollectorRegistry.Unsafe.asJava(cr)))
   } yield new SafeUnlabelledHistogram[F, Unit](out, _ => List.empty).label(())
@@ -21,7 +27,7 @@ object Histogram {
    *  or else `label` will fail
    * FUTURE IMPROVEMENT: Size these lists to make this safe.
    */
-  def construct[F[_]: Sync, A](
+  def construct[F[_]: Sync: Clock, A](
     cr: CollectorRegistry, 
     name: String, 
     help: String, 
@@ -40,7 +46,7 @@ object Histogram {
    * Unsafe Conversion as labels may not align so `label` operation
    * can fail
    */
-  final class UnlabelledHistogram[F[_]: Sync, A] private[epimetheus](
+  final class UnlabelledHistogram[F[_]: Sync: Clock, A] private[epimetheus](
     private val c: JHistogram, 
     private val f: A => List[String]
   ) {
@@ -48,7 +54,7 @@ object Histogram {
       Sync[F].delay(c.labels(f(a):_*)).map(new Histogram[F](_))
   }
 
-  final class SafeUnlabelledHistogram[F[_]: Sync, A] private[epimetheus](
+  final class SafeUnlabelledHistogram[F[_]: Sync: Clock, A] private[epimetheus](
     private val c: JHistogram, 
     private val f: A => List[String]
   ) {
--- core/src/main/scala/io/chrisdavenport/epimetheus/Summary.scala
+++ core/src/main/scala/io/chrisdavenport/epimetheus/Summary.scala
@@ -3,13 +3,18 @@ package io.chrisdavenport.epimetheus
 import cats.implicits._
 import cats.effect._
 import io.prometheus.client.{Summary => JSummary}
+import scala.concurrent.duration._
 
-final class Summary[F[_]: Sync] private (private val s: JSummary.Child){
+final class Summary[F[_]: Sync: Clock] private (private val s: JSummary.Child){
   def observe(d: Double): F[Unit] = Sync[F].delay(s.observe(d))
+  def timed[A](fa: F[A], unit: TimeUnit): F[A] = for {
+    start <- Clock[F].monotonic(unit)
+    out <- Sync[F].guarantee(fa)(Clock[F].monotonic(unit).flatMap(now => observe((now - start).toDouble)))
+  } yield out
 }
 
 object Summary {
-  def buildQuantiles[F[_]: Sync](cr: CollectorRegistry, name: String, help: String, quantiles: (Double, Double)*): F[Summary[F]] = for {
+  def buildQuantiles[F[_]: Sync: Clock](cr: CollectorRegistry, name: String, help: String, quantiles: (Double, Double)*): F[Summary[F]] = for {
     c1 <- Sync[F].delay(JSummary.build().name(name).help(help))
     c <- Sync[F].delay(quantiles.foldLeft(c1){ case (c, q) => c.quantile(q._1, q._2)})
     out <- Sync[F].delay(c.register(CollectorRegistry.Unsafe.asJava(cr)))
@@ -21,7 +26,7 @@ object Summary {
    *  or else `label` will fail
    * FUTURE IMPROVEMENT: Size these lists to make this safe.
    */
-  def construct[F[_]: Sync, A](
+  def construct[F[_]: Sync: Clock, A](
     cr: CollectorRegistry, 
     name: String, 
     help: String, 
@@ -41,7 +46,7 @@ object Summary {
    * Unsafe Conversion as labels may not align so `label` operation
    * can fail
    */
-  final class UnlabelledSummary[F[_]: Sync, A] private[epimetheus](
+  final class UnlabelledSummary[F[_]: Sync: Clock, A] private[epimetheus](
     private val c: JSummary, 
     private val f: A => List[String]
   ) {
@@ -49,7 +54,7 @@ object Summary {
       Sync[F].delay(c.labels(f(a):_*)).map(new Summary[F](_))
   }
 
-  final class SafeUnlabelledSummary[F[_]: Sync, A] private[epimetheus](
+  final class SafeUnlabelledSummary[F[_]: Sync: Clock, A] private[epimetheus](
     private val c: JSummary, 
     private val f: A => List[String]
   ) {
