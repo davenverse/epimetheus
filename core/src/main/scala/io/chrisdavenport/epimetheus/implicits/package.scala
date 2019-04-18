5d8e85aba1642fbc1876bd2a11d8ea33b23ce52f
Chris Davenport <chris@christopherdavenport.tech>
2019-04-18 13:35:56 -0700

    Add Gauge Convenience through privately adding to objects

--- core/src/main/scala/io/chrisdavenport/epimetheus/implicits/package.scala
+++ core/src/main/scala/io/chrisdavenport/epimetheus/implicits/package.scala
@@ -1,6 +1,8 @@
 package io.chrisdavenport.epimetheus
 
 import io.chrisdavenport.epimetheus.syntax.all
+import io.chrisdavenport.epimetheus.syntax.GaugeSyntaxBincompat1
 
 package object implicits
   extends all
+  with GaugeSyntaxBincompat1
--- core/src/main/scala/io/chrisdavenport/epimetheus/syntax/gauge.scala
+++ core/src/main/scala/io/chrisdavenport/epimetheus/syntax/gauge.scala
@@ -3,16 +3,17 @@ package io.chrisdavenport.epimetheus.syntax
 import io.chrisdavenport.epimetheus.Gauge
 import cats.effect._
 
-trait gauge extends GaugeSyntaxBincompat1
+trait gauge
 
-private[syntax] trait GaugeSyntaxBincompat1 {
+// Remove On Next Version
+private[epimetheus] trait GaugeSyntaxBincompat1 {
   implicit class BracketGaugeOps[E, F[_]: Bracket[?[_], E], A](private val gauge: Gauge[F]){
     def incIn(fa: F[A]): F[A] = Gauge.incIn(gauge, fa)
     def incByIn(fa: F[A], i: Double): F[A] = Gauge.incByIn(gauge, fa, i)
     def decIn(fa: F[A]): F[A] = Gauge.decIn(gauge, fa)
     def decByIn(fa: F[A], i: Double): F[A] = Gauge.decByIn(gauge, fa, i)
   }
-
 }
 
 object gauge extends gauge
+  with GaugeSyntaxBincompat1
