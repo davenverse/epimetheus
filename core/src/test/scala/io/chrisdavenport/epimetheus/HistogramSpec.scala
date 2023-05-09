481a1ea067047d03e17e212ec712af6e3e620ce8
alexcardell <29524087+alexcardell@users.noreply.github.com>
2023-05-09 16:56:03 +0100

    Reformat exemplar tests

--- core/src/test/scala/io/chrisdavenport/epimetheus/HistogramSpec.scala
+++ core/src/test/scala/io/chrisdavenport/epimetheus/HistogramSpec.scala
@@ -22,22 +22,30 @@ class HistogramSpec extends munit.CatsEffectSuite {
   }
 
   test("Histogram: observe with exemplars (map)") {
-    val test = for {
+    val setup = for {
       cr <- CollectorRegistry.build[IO]
       h <- Histogram.noLabels(cr, Name("boo"), "Boo ")
-      _ <- h.observeWithExemplar(0.1, Map("exemplar1" -> "value1"))
     } yield h
+
+    val test = for {
+      h <- setup
+      _ <- h.observeWithExemplar(0.1, Map("exemplar1" -> "value1"))
+    } yield ()
 
     test.attempt.map(_.isRight).assert
 
   }
 
   test("Histogram: observe with exemplars (tuples)") {
-    val test = for {
+    val setup = for {
       cr <- CollectorRegistry.build[IO]
       h <- Histogram.noLabels(cr, Name("boo"), "Boo ")
-      _ <- h.observeWithExemplar(0.1, "exemplar1" -> "value1")
     } yield h
+
+    val test = for {
+      h <- setup
+      _ <- h.observeWithExemplar(0.1, "exemplar1" -> "value1")
+    } yield ()
 
     test.attempt.map(_.isRight).assert
 
